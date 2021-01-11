19b0d31de5a3fcfb82110029758d63406415f312
Hardy <hardybailey87@gmail.com>
2021-01-10 23:04:28 -0600

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlBD;
 if(process.env.NODE_ENV==='dev'){
     urlBD='mongodb://localhost:27017/cafe';
 }else{
-    urlBD='mongodb+srv://Shinobi:u4iehxqcxXKKVxRk@cluster0.pd843.mongodb.net/cafe'
+    urlBD=process.env.MONGO_URI
 }
 
 process.env.URLDB=urlBD;
