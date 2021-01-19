5755d0562e5d1448f9642449a1351836e6b1f032
Hardy <hardybailey87@gmail.com>
2021-01-19 09:04:32 -0600

    Prueba de cambio de variable

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,11 @@ process.env.SEED=process.env.SEED ||'SEED_DEV';
 process.env.NODE_ENV=process.env.NODE_ENV || 'dev'
 let urlBD;
 
-if(process.env.NODE_ENV==='dev'){
-    urlBD='mongodb://localhost:27017/cafe';
-}else{
+if(process.env.NODE_ENV==='production'){
     urlBD=process.env.MONGO_URI
+    
+}else{
+    urlBD='mongodb://localhost:27017/cafe';
 }
 
 process.env.URLDB=urlBD;
